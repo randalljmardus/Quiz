7617a0dc9f30f46f183a4da1869482e5857831e7
Randall Mardus <randalljmardus@gmail.com>
2016-04-19 13:29:14 -0400

    added arrays of questions, answers

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -12,12 +12,32 @@ class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     
+    let questions: [String] = ["From what is cognac made?", "What is 7+7?", "What is the capitol of Vermont?"]
+    
+    let answers: [String] = ["Grapes", "14", "Montpelier"]
+    
+    var currentQuestionIndex: Int = 0
+    
     @IBAction func showNextQuestion(sender: AnyObject) {
+        ++currentQuestionIndex
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
         
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
     }
     
     @IBAction func showAnswer(sender: AnyObject) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
         
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
     }
 }
 
